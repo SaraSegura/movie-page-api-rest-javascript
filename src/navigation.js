fcf296e91783b0bc5892ca33f845f41064127fe0
sara segura <sarasegura224@gmail.com>
2023-09-06 14:26:37 -0500

    Se agregaron estilos al diseño.

--- src/navigation.js
+++ src/navigation.js
@@ -122,7 +122,7 @@ function trendsPage() {
   headerSection.classList.remove("header-container--long");
   headerSection.style.background = "";
   arrowBtn.classList.remove("inactive");
-  arrowBtn.classList.remove("header-arrow--white");
+  arrowBtn.classList.add("header-arrow--white");
   headerTitle.classList.add("inactive");
   headerCategoryTitle.classList.remove("inactive");
   searchForm.classList.add("inactive");
