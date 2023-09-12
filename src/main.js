916b5ffac23a123b64db99423e92bd0de203cf3f
sara segura <sarasegura224@gmail.com>
2023-09-12 15:46:33 -0500

    clase 16

--- src/main.js
+++ src/main.js
@@ -8,6 +8,30 @@ const api = axios.create({
   },
 });
 
+function likedMoviesList() {
+  const item = JSON.parse(localStorage.getItem("liked_movies"));
+  let movies;
+
+  if (item) {
+    movies = item;
+  } else {
+    movies = {};
+  }
+  return movies;
+}
+
+function likeMovie(movie) {
+  const likedMovies = likedMoviesList();
+
+  if (likedMovies[movie.id]) {
+    likedMovies[movie.id] = undefined;
+  } else {
+    likedMovies[movie.id] = movie;
+  }
+
+  localStorage.setItem("liked_movies", JSON.stringify(likedMovies));
+}
+
 // Utils
 
 const lazyLoader = new IntersectionObserver((entries) => {
@@ -54,7 +78,7 @@ function createMovies(
     movieBtn.classList.add("movie-btn");
     movieBtn.addEventListener("click", () => {
       movieBtn.classList.toggle("movie-btn--liked");
-      //DEBERIAMOS AGREGAR LA PELICULAS A LS
+      likeMovie(movie);
     });
 
     if (lazyLoad) {
